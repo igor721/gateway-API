67c594ce0e8e282492fb76ae90811412d0115873
igor721 <igorgabriel721@gmail.com>
2026-02-20 16:48:01 +0000

    Final Version

--- gateway.py
+++ gateway.py
@@ -6,11 +6,10 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-# --- SIMULAÇÃO DOS MICROSERVIÇOS (Lendo de arquivos JSON) ---
 
 @app.route('/servico-catalogo/<id>')
 def get_catalogo(id):
-    with open('catalogo.json', 'r', encoding='utf-8') as f:
+    with open('./catalogo.json', 'r', encoding='utf-8') as f:
         dados = json.load(f)
     # Busca o ID no JSON ou retorna erro se não existir
     jogo = dados.get(id)
@@ -18,17 +17,15 @@ def get_catalogo(id):
 
 @app.route('/servico-precos/<id>')
 def get_precos(id):
-    with open('precos.json', 'r', encoding='utf-8') as f:
+    with open('./precos.json', 'r', encoding='utf-8') as f:
         dados = json.load(f)
     preco = dados.get(id)
     return jsonify(preco) if preco else (jsonify({"erro": "Preço não encontrado"}), 404)
 
-# --- O API GATEWAY (Mantém a mesma lógica de agregação) ---
 
 @app.route('/produto/<id>')
 def api_gateway(id):
     try:
-        # O Gateway continua chamando as URLs, mas agora elas retornam dados dos arquivos
         res_cat = requests.get(f'http://127.0.0.1:5000/servico-catalogo/{id}')
         if res_cat.status_code != 200:
             return jsonify({"erro": "Erro no serviço de catálogo"}), 404
@@ -40,7 +37,6 @@ def api_gateway(id):
         dados_cat = res_cat.json()
         dados_pre = res_pre.json()
         
-        # Agregação final
         resposta_final = {
             "id_original": id,
             "produto": dados_cat['nome'],
